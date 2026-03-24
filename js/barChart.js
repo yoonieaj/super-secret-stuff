306203c27086d05782f180644dc3b46a540035d8
yoonieaj <yoonie.jang@mail.utoronto.ca>
2026-03-24 02:39:01 -0400

    changes

--- js/barChart.js
+++ js/barChart.js
@@ -1,5 +1,5 @@
 // todo: make these not constants anymore
-const WIDTH = 600;
+const WIDTH = 900;
 const HEIGHT = 600;
 const AWARDCLASSES = [
     'selectedRestaurants',
@@ -8,6 +8,13 @@ const AWARDCLASSES = [
     'twoStar',
     'threeStar'
 ]
+const COLORSCHEME = {
+    'selectedRestaurants': '#70a5b0',
+    'bibGourmand': '#8ecac0',
+    'oneStar': '#f3cf91',
+    'twoStar': '#f48383',
+    'threeStar': '#cd4a73'
+}
 
 class BarChart {
 
@@ -35,8 +42,9 @@ class BarChart {
         vis.margin = { top: 10, right: 50, bottom: 10, left: 50 };
         vis.barPadding = 150;
         vis.barPaddingY = 20;
-        vis.width = parentRect.width - vis.margin.left - vis.margin.right;
+        vis.width = WIDTH - vis.margin.left - vis.margin.right;
         vis.height = HEIGHT - vis.margin.top - vis.margin.bottom;
+        vis.tooltipHeight = 250;
         vis.transitionDuration = 1000
 
         // add the event listeners for the checkboxes
@@ -55,11 +63,15 @@ class BarChart {
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
             .attr("width", vis.width + vis.margin.left + vis.margin.right)
             .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
-        
+
         // add x-axis group
         vis.svg.append("g")
             .attr("class", "x-axis")
             .attr("transform", `translate(${vis.barPadding}, ${vis.barPaddingY})`)
+
+        vis.tooltip = d3.select("body").append("div")
+            .attr("id", "country-tooltip")
+            .style("opacity", 0)
 
         vis.wrangleData();
     }
@@ -123,7 +135,7 @@ class BarChart {
         })
 
         const cutoff = 33       // anything after 33rd entry will be labelled as "other" initially
-        vis.countryFilteredArray = vis.countryTotalArray.slice(0,cutoff)
+        vis.countryFilteredArray = vis.countryTotalArray.slice(0, cutoff)
         const others = vis.countryTotalArray.slice(cutoff)
         let otherSelected = 0
         let otherBG = 0;
@@ -162,16 +174,15 @@ class BarChart {
             }
             return sum
         })
-
-        var x = d3.scaleLinear()
+        vis.x = d3.scaleLinear()
             .domain([0, maxRestaurants])
-            .range([0, vis.width - vis.barPadding - 10]);
-        
+            .range([0, WIDTH - vis.barPadding - 10]);
+
         console.log("vis height", vis.height)
-        var y = d3.scaleBand()
+        vis.y = d3.scaleBand()
             .range([0, vis.height])
             .domain(vis.countryFilteredArray.map(d => d.country))
-            .padding(.3);
+            .padding(.1);
 
         var stackedData = d3.stack()
             .keys(AWARDCLASSES)
@@ -180,19 +191,9 @@ class BarChart {
             })
             (vis.countryFilteredArray)
 
-            console.log("stacked data", stackedData)
-
-        const colorScheme = {
-            'selectedRestaurants': '#c3d0ea',
-            'bibGourmand': '#77bbbb',
-            'oneStar': '#f6dbb8',
-            'twoStar': '#faa16d',
-            'threeStar': '#f33b3b'
-        }
-
         // draw x axis
         vis.xAxis = d3.axisTop()
-            .scale(x)
+            .scale(vis.x)
             .ticks(4);
 
         console.log("xaxis", vis.xAxis)
@@ -206,48 +207,102 @@ class BarChart {
 
         let stackGroups = vis.svg.selectAll(".bar-group")
             .data(stackedData)
-        
+
         console.log("stacked data", stackedData)
+
 
         let bars = stackGroups.enter().append("g")
             .attr("class", "bar-group")
             .merge(stackGroups)
-            .attr("fill", function (d) { console.log("color", d.key); return colorScheme[d.key]; })
+            .attr("fill", function (d) { console.log("color", d.key); return COLORSCHEME[d.key]; })
 
             .selectAll("rect")
             .data(function (d) {
                 // console.log("function d", d) 
-                return d; })
+                return d;
+            })
 
         bars.enter()
             .append("rect")
             .attr("fill", function (d) {
-                return colorScheme[d.key]; 
+                return COLORSCHEME[d.key];
+            })
+            .attr("x", function (d) {
+                return vis.barPadding;
+            })
+            .on('mouseover', function (event, x) {
+                // highlight selected bar
+                d3.selectAll("rect")
+                    .style("opacity", function (d) {
+                        if (d.data.country === x.data.country) {
+                            return 1
+                        }
+                        return 0.25
+                    })
+                d3.selectAll(".country-label")
+                    .style("opacity", function (d) {
+                        if (d.country === x.data.country) {
+                            return 1
+
+                        }
+
+                        for (const award of vis.includedAwards) {
+                            if (d[award] > 0) {
+                                return 0.25
+                            }
+                        }
+                        return 0;
+
+                    })
+
+                // appear the tooltip
+                vis.countryTooltip(event, x, vis)
+            })
+            .on('mouseout', function (event, x) {
+                d3.selectAll("rect")
+                    .style("opacity", 1)
+                d3.selectAll(".country-label")
+                    .style("opacity", function (d) {
+                        {
+                            for (const award of vis.includedAwards) {
+                                if (d[award] > 0) {
+                                    return 1
+                                }
+                            }
+                            return 0;
+                        }
+                    })
+                vis.tooltip
+                    .style("opacity", 0)
+                    .style("left", 0)
+                    .style("top", 0)
+                    .html(``);
             })
 
             .merge(bars)
             .attr("y", function (d, i) {
-                return y(vis.countryFilteredArray[i].country) + vis.barPaddingY;
+                return vis.y(d.data.country) + vis.barPaddingY;
             })
 
-            .attr("height", y.bandwidth())
+            .attr("height", vis.y.bandwidth())
             .transition()
             .duration(vis.transitionDuration)
-            .attr("width", function (d) { 
-                return x(d[1]) - x(d[0]); 
+            .attr("width", function (d) {
+                return vis.x(d[1]) - vis.x(d[0]);
             })
             .attr("x", function (d) {
-                return x(d[0]) + vis.barPadding;
+                return vis.x(d[0]) + vis.barPadding;
             })
+
 
         bars.exit().remove()
         stackGroups.exit().remove()
-        
+
         let labels = vis.svg.selectAll(".country-label")
-            .data(vis.countryFilteredArray, function(d) {
+            .data(vis.countryFilteredArray, function (d) {
                 return d.country
             })
-        
+
         // add / update labels
         labels.enter().append("text")
             .attr("class", "country-label")
@@ -257,9 +312,9 @@ class BarChart {
             .merge(labels)
             .transition()
             .duration(isUpdate ? vis.transitionDuration : 0)
-            .text(d => {return d.country})
-            .attr("y", d => y(d.country) + 10 + vis.barPaddingY)
-            .attr("opacity", d => {
+            .text(d => { return d.country })
+            .attr("y", d => vis.y(d.country) + 10 + vis.barPaddingY)
+            .style("opacity", d => {
                 for (const award of vis.includedAwards) {
                     if (d[award] > 0) {
                         return 1
@@ -268,6 +323,42 @@ class BarChart {
                 return 0;
             })
         labels.exit().remove()
+    }
+
+    countryTooltip(event, x, vis) {
+        let yCoord = event.pageY - 10;
+        let bodyRect = document.getElementById('body').getBoundingClientRect()
+        yCoord = Math.min(yCoord, bodyRect.height - vis.tooltipHeight)
+
+        let countryData = vis.countryFilteredArray.filter((d) => d.country === x.data.country)[0]
+        console.log("countrydata", countryData)
+        // let maxRestaurants = d3.max(vis.countryFilteredArray, (d) => {
+        let sum = 0
+        for (const award of vis.includedAwards) {
+            sum += countryData[award];
+        }
+        console.log(sum)
+        // })
+        // let xCoord = vis.x()
+        vis.tooltip
+            .style("opacity", 1)
+            .style("left", vis.x(sum) + 200 + "px")
+            .style("top", yCoord + "px")
+            .html(`
+                    <div>
+                        <h3>${x.data.country} (${sum} restaurants)</h3>
+                        <div class="row-container">
+                        <div>
+                            <p>${x.data.selectedRestaurants} selected restaurants</p>
+                            <p>${x.data.bibGourmand} bib gourmand</p>
+                            <p>${x.data.oneStar} one star</p>
+                            <p>${x.data.twoStar} two star</p>
+                            <p>${x.data.threeStar} three star</p>
+                        </div>
+                        <div>akdfhaoiduf</div>
+                        </div>
+
+                    </div>`);
     }
 
 };
