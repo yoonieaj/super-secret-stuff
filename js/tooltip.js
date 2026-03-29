19d9833885dbf484e63fbabc845e445173b70ed5
yoonieaj <yoonie.jang@mail.utoronto.ca>
2026-03-29 00:13:37 -0400

    remove keyword count

--- js/tooltip.js
+++ js/tooltip.js
@@ -126,14 +126,10 @@ class Tooltip {
             .html(`<div>
                     <div class="tooltip-text-container">
                         <p>top regional keywords: </p>
-                        <span>${countryTotal.length > 0 ? countryTotal[0].cuisine: ''} 
-                        ${countryTotal.length > 0 ? '(' + countryTotal[0].count + ')': ''}</span>
-                        <span>${countryTotal.length > 1 ? ', ' + countryTotal[1].cuisine: ''} 
-                        ${countryTotal.length > 1 ? '(' + countryTotal[1].count + ')': ''}</span>
-                        <span>${countryTotal.length > 2 ? ', ' + countryTotal[2].cuisine: ''} 
-                        ${countryTotal.length > 2 ? '(' + countryTotal[2].count + ')' : ''}</span>
-                        <span>${countryTotal.length > 3 ? ', ' + countryTotal[3].cuisine : ''} 
-                        ${countryTotal.length > 3 ? '(' + countryTotal[3].count + ')': ''}</span>
+                        <span>${countryTotal.length > 0 ? countryTotal[0].cuisine: ''} </span>
+                        <span>${countryTotal.length > 1 ? ', ' + countryTotal[1].cuisine: ''} </span>
+                        <span>${countryTotal.length > 2 ? ', ' + countryTotal[2].cuisine: ''} </span>
+                        <span>${countryTotal.length > 3 ? ', ' + countryTotal[3].cuisine : ''} </span>
                         
                     </div>
                 </div>`)
